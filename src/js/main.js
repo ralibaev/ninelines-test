e50b62e81db7b2adb8d10e632101a7471e42bf80
ralibaev <ralibaev@bk.ru>
2022-04-10 18:29:24 +0500

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+import './vendor';
+import './helpers';
+import './components/social';
+import {ieFix} from './vendor/ie-fix';
+import {vhFix} from './vendor/vh-fix';
+import {actualYear} from './modules/actualYear';
+import header from './components/header';
+import lazyLoading from './modules/lazyLoading';
+import scrollToAnchor from './modules/scrollToAnchor';
+import Preloader from 'ninelines-preloader';
+
+ieFix();
+vhFix();
+actualYear();
+scrollToAnchor.init();
+
+header.init();
+lazyLoading.init();
+Preloader.init()
+  .then(() => {
+    document.querySelector('.preloader').style.display = 'none';
+    showPage();
+  });
